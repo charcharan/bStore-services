50c569d0c4538dee0ff1c28efb0843bf9b281d28
ready-to-learn <jaiindira@gmail.com>
2021-01-27 21:44:50 +0530

    Changes to parse response and added comments

--- src/main/java/com/bteam/bstore/beans/ScannedItem.java
+++ src/main/java/com/bteam/bstore/beans/ScannedItem.java
@@ -1,19 +1,30 @@
 package com.bteam.bstore.beans;
 
-public class ScannedItem {
-	private final long id;
+public class ScannedItem implements Comparable<ScannedItem> {
+	
+	private final double confidence;
 	private final String name;
 
-	public ScannedItem(Long id, String name) {
-	        this.id = id;
+	public ScannedItem(Double confidence2, String name) {
+	        this.confidence = confidence2;
 	        this.name = name;
 	    }
 
-	public long getId() {
-		return this.id;
+	public double getConfidence() {
+		return this.confidence;
 	}
 
 	public String getName() {
 		return this.name;
 	}
+	
+	//To sort the items in descending order based on confidence of item
+	public int compareTo(ScannedItem item){  
+		if(this.confidence==item.confidence)  
+		return 0;  
+		else if(this.confidence<item.confidence)  
+		return 1;  
+		else  
+		return -1;  
+		}  
 }
